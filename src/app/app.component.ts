e745ad6ad1b16453da4b053ea54ede11856e7057
Mwelwa Nkuta <mwelwankuta@gmail.com>
2024-01-17 12:43:54 +0200

    feat(game-loop): updating snake position, creating new food

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,123 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { CellComponent } from './cell/cell.component';
+import { Position } from './position';
+
+type MoveDirection = 'ArrowLeft' | 'ArrowRight' | 'ArrowUp' | 'ArrowDown';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <main>
+      <h1>Score: {{ this.score }}</h1>
+      <div
+        class="board"
+        [ngStyle]="{ 'grid-template-columns': templateColumnStyle }"
+      >
+        <app-cell
+          *ngFor="let cell of cells"
+          [cell]="cell"
+          [foodPosition]="foodPosition"
+          [snakePosition]="snakePosition"
+          (onAteFood)="onAteFood()"
+        ></app-cell>
+      </div>
+    </main>
+  `,
+  styleUrl: './app.component.css',
+  imports: [CommonModule, CellComponent],
 })
-export class AppComponent {
-  title = 'task-tracker';
+export class AppComponent implements OnInit {
+  score = 0;
+  gridSize = 20;
+  interval = 500;
+  cells: Position[] = [];
+  snakePosition = [
+    { x: this.gridSize / 2, y: this.gridSize / 2 },
+    { x: this.gridSize / 2 + 1, y: this.gridSize / 2 },
+  ];
+
+  direction: MoveDirection = 'ArrowDown';
+
+  foodPosition = {
+    x: Math.floor(Math.random() * this.gridSize),
+    y: Math.floor(Math.random() * this.gridSize),
+  };
+  templateColumnStyle = new Array(this.gridSize).fill('1fr').join(' ');
+
+  ngOnInit(): void {
+    this.generateBoard();
+    // determine number of columns for styles
+
+    if (
+      this.snakePosition[0].x >= this.gridSize ||
+      this.snakePosition[0].y >= this.gridSize
+    ) {
+      alert('game over');
+      return;
+    }
+
+    setInterval(() => {
+      this.updateGame();
+    }, this.interval);
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    this.direction = event.key as MoveDirection;
+  }
+
+  onAteFood() {
+    this.score += 10;
+    this.interval -= 50;
+    this.snakePosition.push({
+      x: this.snakePosition[this.snakePosition.length - 1].x,
+      y: this.snakePosition[this.snakePosition.length - 1].y,
+    });
+    this.foodPosition = {
+      x: Math.floor(Math.random() * this.gridSize),
+      y: Math.floor(Math.random() * this.gridSize),
+    };
+  }
+
+  updateGame() {
+    const newSnake = [...this.snakePosition];
+    switch (this.direction) {
+      case 'ArrowRight':
+        newSnake.unshift({
+          x: newSnake[0].x,
+          y: newSnake[0].y + 1,
+        });
+        break;
+      case 'ArrowLeft':
+        newSnake.unshift({
+          x: newSnake[0].x,
+          y: newSnake[0].y - 1,
+        });
+        break;
+      case 'ArrowUp':
+        newSnake.unshift({
+          x: newSnake[0].x - 1,
+          y: newSnake[0].y,
+        });
+        break;
+      case 'ArrowDown':
+        newSnake.unshift({
+          x: newSnake[0].x + 1,
+          y: newSnake[0].y,
+        });
+        break;
+    }
+    newSnake.pop();
+    this.snakePosition = newSnake;
+  }
+
+  generateBoard() {
+    for (let row = 0; row < this.gridSize; row++) {
+      for (let col = 0; col < this.gridSize; col++) {
+        this.cells.push({ x: row, y: col });
+      }
+    }
+  }
 }
---
+++ src/app/cell/cell.component.ts
@@ -0,0 +1,86 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { Position } from '../position';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cell',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <div>
+      <div [ngSwitch]="cellStatus">
+        <div
+          *ngSwitchCase="'food'"
+          [ngClass]="{ cell: true, food: true }"
+        ></div>
+
+        <div
+          *ngSwitchCase="'snake'"
+          [ngClass]="{ cell: true, snake_body: true }"
+        ></div>
+
+        <div
+          *ngSwitchCase="'snake-head'"
+          [ngClass]="{ cell: true, snake_head: true }"
+        ></div>
+
+        <div *ngSwitchDefault [ngClass]="{ cell: true }"></div>
+      </div>
+    </div>
+  `,
+  styleUrl: './cell.component.css',
+})
+export class CellComponent implements OnChanges {
+  @Input() cell!: Position;
+  @Input() foodPosition!: Position;
+  @Input() snakePosition!: Position[];
+  @Output() onAteFood = new EventEmitter();
+
+  cellStatus: 'cell' | 'food' | 'snake' | 'snake-head' = 'cell';
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const change = changes['snakePosition'].currentValue;
+    if (!change) return;
+    this.snakePosition = change;
+
+    const cellX = this.cell.x;
+    const cellY = this.cell.y;
+
+    const isSnake = this.snakePosition.some(
+      (snake) => snake.x == cellX && snake.y == cellY
+    );
+    const isSnakeHead =
+      this.snakePosition[0].x == cellX && this.snakePosition[0].y == cellY;
+    const isFood = cellX == this.foodPosition.x && cellY == this.foodPosition.y;
+
+    // check if on food
+    if (
+      this.snakePosition[0].x == this.foodPosition.x &&
+      this.snakePosition[0].y == this.foodPosition.y
+    ) {
+      this.foodPosition = { x: -1, y: -1 };
+      this.onAteFood.emit();
+    }
+
+    if (isSnake) {
+      this.cellStatus = 'snake';
+    } else {
+      this.cellStatus = 'cell';
+    }
+    if (isSnakeHead) {
+      this.cellStatus = 'snake-head';
+    }
+    if (isFood) {
+      this.cellStatus = 'food';
+    }
+  }
+}
